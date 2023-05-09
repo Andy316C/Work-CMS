8a802225e3c0770b2190774b6331d5a1eb3b28c6
andrew clark <andy-clark316@hotmail.com>
2023-05-09 16:48:10 +1000

    added missing comma, causing data issue

--- sql_data/seeds.sql
+++ sql_data/seeds.sql
@@ -16,7 +16,7 @@ INSERT INTO employee (first_name, last_name, role_id, manager_id)
 VALUES ("David", "sinclair", 3,1 ),
        ("Michael", "mcintosh", 2,2),
        ("Sarah", "selblair", 2,3 ),
-       ("Paul", "dims", 1,4)
+       ("Paul", "dims", 1,4),
        ("Michelle", "yarray", 1,3),
        ("Monica", "abdulla", 4,2),
        ("Larry", "clark", 3,3),
